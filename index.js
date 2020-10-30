f5c6a04fd2690e92a8bd061a87a117590f62bd75
sebseb7 <sebgreenbus@gmail.com>
2020-10-30 22:49:17 +0000

    facebook support

--- index.js
+++ index.js
@@ -8,9 +8,14 @@ const port = 2222;
 const io = require('@pm2/io')
 
 const activeGrabs = io.metric({
-	name: 'active grabs',
+	name: 'active grabs yt',
 })
 activeGrabs.set(0);
+
+const activeGrabsFB = io.metric({
+	name: 'active grabs fb',
+})
+activeGrabsFB.set(0);
 
 
 var relays = {};
@@ -67,7 +72,7 @@ function start_process(id,source,target) {
 function start_fm_process(id,target) {
 	
 
-	const cmdline = '-init_hw_device qsv=hw -hwaccel qsv -hide_banner -re -c:v h264_qsv -c:a libfdk_aac -i pipe:0 -g 60 -c:v h264_qsv -c:a libfdk_aac -b:v 2800k -maxrate 2800k -bufsize 3000k -b:a 128k -bf 2 -preset fast -strict -2 -f flv '+target
+	const cmdline = '-init_hw_device qsv=hw -hwaccel qsv -gpu_copy on -hide_banner -re -c:v h264_qsv -c:a aac -i pipe:0 -g 60 -c:v h264_qsv -c:a aac -b:v 2800k -maxrate 2800k -bufsize 3000k -b:a 128k -bf 2 -preset fast -strict -2 -f flv '+target
 	
 	console.log('start ffmpeg '+cmdline);
 
@@ -119,7 +124,99 @@ app.post('/add_relay', (req, res) => {
 app.post('/delete_relay', (req, res) => {
 	delete(relays[req.body.id]);
 	relay_processes[req.body.id].streamlink.kill('SIGKILL');
+	relay_processes[req.body.id].ffmpeg.kill('SIGKILL');
 	activeGrabs.set(Object.keys(relays).length);
+	res.send('ok');
+});
+
+var relays_fb = {};
+var relay_processes_fb = {};
+
+function start_process_fb(id,original,source,target) {
+
+	console.log('start fb ffmpeg');
+
+	relays_fb[id]={source:original,target:target};
+	if(! relay_processes_fb[id]) relay_processes_fb[id]={};
+
+	activeGrabsFB.set(Object.keys(relays_fb).length);
+
+//	const cmdline = '-re -f dash -i '+source+' -c:v copy -c:a aac -f flv '+target;
+//	console.log(cmdline);
+
+	var process_sl = spawn('ffmpeg',('-re -f dash -i '+source+' -c:v copy -c:a aac -f flv '+target).split(' '));
+
+	relay_processes_fb[id].ffmpeg = process_sl;
+
+	process_sl.stdout.on('data', (data) => {
+		console.log('ffmpeg fb stdout: '+data);
+	});
+	process_sl.on('close', (code) => {
+		if (code !== 0) {
+			console.log('ffmpeg fb process exited with code '+code);
+		}else{
+			console.log('ffmpeg fb process exited clean');
+		}
+		try {
+			relay_processes_fb[id].ffmpeg.stdin.end();
+		} catch (err) {
+			console.log('pipe end error: '+err);
+		}
+		if(relays[id]) {
+			console.log('restart ffmpeg fb');
+			resolve_url(id,original,target);
+		}
+	});
+	process_sl.stderr.on('data', (data) => {
+		if(relays_fb[id]) relays_fb[id].ffmpeg=data.toString();
+	});
+	process_sl.on('error', (err) => {
+		console.log('ffmpeg fb errored. '+err);
+	});
+}
+
+function resolve_url(id,value,target)
+{
+	console.log('resolve:'+value);
+	var ls = spawn('youtube-dl', ['-g',value],{shell: false});
+
+	ls.stdout.on('data', (data) => {
+		console.log('stdout: '+data);
+		start_process_fb(id,value,data.toString().replace(/(\r\n|\n|\r)/gm, ""),target);
+
+	});
+
+	ls.stderr.on('data', (data) => {
+		console.log('stderr: '+data);
+	});
+
+	ls.on('close', (code) => {
+		console.log(`child process exited with code ${code}`);
+	});
+	ls.on('error', (code,text) => {
+		console.log(`child process exited with error ${code} ${text}`);
+	});
+}
+
+
+
+app.get('/relay_state_fb', (req, res) => {
+	res.send(JSON.stringify(relays_fb));
+});
+
+app.post('/add_relay_fb', (req, res) => {
+	
+	var id = Math.random().toString(36).substr(2, 9);
+
+	resolve_url(id,req.body.source,req.body.target);
+
+	res.send('ok');
+});
+
+app.post('/delete_relay_fb', (req, res) => {
+	delete(relays_fb[req.body.id]);
+	relay_processes_fb[req.body.id].ffmpeg.kill('SIGKILL');
+	activeGrabsFB.set(Object.keys(relays_fb).length);
 	res.send('ok');
 });
 
@@ -130,4 +227,5 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
 	console.log(`App listening at http://localhost:${port}`)
 });
+
 
